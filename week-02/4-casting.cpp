6108fcce4274cb324c30276cb30018c697ab845d
Ashu <ashuprince7337@gmail.com>
2024-04-21 16:34:00 +0530

    changing the dot with hyphen

---
+++ week-02/4-casting.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    // Q1. implicit casting
+    // int to float
+    int num1 = 10;
+    float num2 = 5.5;
+    float result = num1 + num2;
+    cout << result << endl;
+
+    // // char to int
+    // char ch = 'A';
+    // int a = ch + 1;
+    // cout << a << endl;
+
+    return 0;
+}
