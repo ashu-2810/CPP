72e6535cd62e3947315da92d7d44abde91c94128
Ashu <ashuprince7337@gmail.com>
2024-07-22 22:28:52 +0530

    22-07-2024

--- week-03/HOMEWORK/Q6.cpp
+++ week-03/HOMEWORK/Q6.cpp
@@ -8,7 +8,7 @@ using namespace std;
 int Duplicate(vector<int> &nums)
 {
     sort(nums.begin(), nums.end());
-    for (int i = 0; i < nums.size()-1; i++)
+    for (int i = 0; i < nums.size() - 1; i++)
     {
         if (nums[i] == nums[i + 1])
         {
@@ -17,11 +17,42 @@ int Duplicate(vector<int> &nums)
     }
     return -1;
 }
+// ---------------------------OR---------------------------
+
+// negative marking method  TC- O(n)  SC- O(1)
+int findDuplicate(vector<int> &nums)
+{
+    int ans = -1;
+    for (int i = 0; i < nums.size(); i++)
+    {
+        int index = abs(nums[i]);
+        if (nums[index] < 0)
+        {
+            return i;
+        }
+        nums[index] *= -1;
+    }
+    return ans;
+}
+
+// ------------------------------OR-----------------------
+
+// positioning method
+int pleaseFindDuplicate(vector<int> &nums)
+{
+    while (nums[0] != nums[nums[0]])
+    {
+        swap(nums[0], nums[nums[0]]);
+    }
+    return nums[0];
+}
 
 int main()
 {
-    vector<int> nums = {1, 3, 4, 4, 2};
+    vector<int> nums = {1, 3, 2, 4, 2};
     cout << Duplicate(nums) << endl;
+    cout << findDuplicate(nums) << endl;
+    cout << pleaseFindDuplicate(nums) << endl;
 
     return 0;
 }
