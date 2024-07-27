4daa615960e6aaa88ee51f1ce51222538150f9e5
Ashu <ashuprince7337@gmail.com>
2024-07-27 10:56:49 +0530

    27-07-2024

--- week-03/HOMEWORK/Q11.cpp
+++ week-03/HOMEWORK/Q11.cpp
@@ -3,15 +3,62 @@
 #include <vector>
 using namespace std;
 
-void spiralPrint(vector<vector<int>> &v)
+vector<int> spiralPrint(vector<vector<int>> &v)
 {
+    vector<int> ans;
+    int rows = v.size();
+    int cols = v[0].size();
+    int totalElements = rows * cols;
 
+    int startrow = 0;
+    int endcol = cols - 1;
+    int endrow = rows - 1;
+    int startcol = 0;
+
+    int count = 0;
+    while (count < totalElements)
+    {
+        // print startrow
+        for (int i = startcol; i <= endcol && count < totalElements; i++)
+        {
+            ans.push_back(v[startrow][i]);
+            count++;
+        }
+        startrow++;
+        // print endcol
+        for (int i = startrow; i <= endrow && count < totalElements; i++)
+        {
+            ans.push_back(v[i][endcol]);
+            count++;
+        }
+        endcol--;
+        // print endrow
+        for (int i = endcol; i >= startcol && count < totalElements; i--)
+        {
+            ans.push_back(v[endrow][i]);
+            count++;
+        }
+        endrow--;
+        // print startcol
+        for (int i = endrow; i >= startrow && count < totalElements; i--)
+        {
+            ans.push_back(v[i][startcol]);
+            count++;
+        }
+        startcol++;
+    }
+    return ans;
 }
 
 int main()
 {
     vector<vector<int>> v = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
-    spiralPrint(v);
+    vector<int> vAns = spiralPrint(v);
+    for (int i = 0; i < vAns.size(); i++)
+    {
+        cout << vAns[i] << " ";
+    }
+    cout << endl;
 
     return 0;
 }
---
+++ week-03/HOMEWORK/Q12.cpp
@@ -0,0 +1,78 @@
+// factorial of a number
+#include <iostream>
+#include <vector>
+using namespace std;
+
+// add two numbers represented by two arrays
+#include <algorithm>
+string calSum(int *a, int n, int *b, int m)
+{
+    int carry = 0;
+    string ans;
+    int i = n - 1;
+    int j = m - 1;
+
+    while (i >= 0 && j >= 0)
+    {
+        int x = a[i] + b[j] + carry;
+        int digit = x % 10;
+        ans.push_back(digit + '0');
+        carry = x / 10;
+        i--, j--;
+    }
+    while (i >= 0)
+    {
+        int x = a[i] + 0 + carry;
+        int digit = x % 10;
+        ans.push_back(digit + '0');
+        carry = x / 10;
+        i--;
+    }
+    while (j >= 0)
+    {
+        int x = 0 + b[j] + carry;
+        int digit = x % 10;
+        ans.push_back(digit + '0');
+        carry = x / 10;
+        j--;
+    }
+    if (carry)
+    {
+        ans.push_back(carry + '0');
+    }
+    while (ans[ans.size() - 1] == '0')
+    {
+        ans.pop_back();
+    }
+
+    reverse(ans.begin(), ans.end());
+    return ans;
+}
+
+vector<int> factorial(int nfact)
+{
+}
+
+int main()
+{
+    // add two numbers represented by two arrays
+    int a[] = {1, 2};
+    int b[] = {2, 1};
+    int n = 2, m = 2;
+    string results = calSum(a, n, b, m);
+    for (auto i : results)
+    {
+        cout << i;
+    }
+    cout << endl;
+
+    int nfact = 10;
+    vector<int> result = factorial(nfact);
+    for (int i = 0; i < result.size(); i++)
+    {
+        cout << result[i];
+    }
+    cout << endl;
+
+    return 0;
+}
