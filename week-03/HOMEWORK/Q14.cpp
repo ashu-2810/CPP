736450bb8d658f78f9b23920597b502489bdf16d
Ashu <ashuprince7337@gmail.com>
2024-07-28 23:49:01 +0530

    28-07-2024

--- week-03/HOMEWORK/Q14.cpp
+++ week-03/HOMEWORK/Q14.cpp
@@ -4,6 +4,7 @@
 #include <limits.h>
 using namespace std;
 
+// method 1 TC- O(n2) SC- O(1)
 double maxAvg(vector<int> &nums, int k)
 {
     int maxsum = INT_MIN;
@@ -22,12 +23,32 @@ double maxAvg(vector<int> &nums, int k)
     return maxavg;
 }
 
+double maxAvgg(vector<int> &nums, int &k)
+{
+    int i = 0, j = k - 1;
+    int sum = 0;
+    for (int y = i; y <= j; y++)
+    {
+        sum += nums[y];
+    }
+    int maxsum = sum;
+    j++;
+    while (j < nums.size())
+    {
+        sum -= nums[i++];
+        sum += nums[j++];
+        maxsum = max(maxsum, sum);
+    }
+    double maxavg = maxsum / (double)k;
+    return maxavg;
+}
+
 int main()
 {
 
     vector<int> nums = {1, 12, -5, -6, 50, 3};
     int k = 4;
-    cout << "maximum average is: " << maxAvg(nums, k) << endl;
+    cout << "maximum average is: " << maxAvgg(nums, k) << endl;
 
     return 0;
 }
