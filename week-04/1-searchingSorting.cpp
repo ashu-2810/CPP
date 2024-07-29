e97b473b18111f001af51bee84a777ba10641421
Ashu <ashuprince7337@gmail.com>
2024-07-30 00:33:40 +0530

    29-07-2024

--- week-04/1-searchingSorting.cpp
+++ week-04/1-searchingSorting.cpp
@@ -1,8 +1,8 @@
-// linear search
 #include <iostream>
 #include <vector>
 using namespace std;
 
+// linear search
 bool linearSearch(vector<int> &nums, int target)
 {
     for (int i = 0; i < nums.size(); i++)
@@ -13,8 +13,35 @@ bool linearSearch(vector<int> &nums, int target)
     return false;
 }
 
+// binary Search
+int binarySearch(vector<int> &brr1, int targets)
+{
+    int i = 0;
+    int j = brr1.size() - 1;
+    int mid = (i + j) / 2;
+    while (i <= j)
+    {
+        if (brr1[mid] == targets)
+        {
+            return mid;
+        }
+        else if (brr1[mid] < targets)
+        {
+            i = mid + 1;
+        }
+        else if (brr1[mid] > targets)
+        {
+            j = mid - 1;
+        }
+        // mid update
+        mid = (i + j) / 2;
+    }
+    return -1;
+}
+
 int main()
 {
+    // linear search
     vector<int> nums = {10, 20, 30, 40, 50, 60, 70, 80, 90};
     int target = 70;
     bool ans = linearSearch(nums, target);
@@ -22,6 +49,11 @@ int main()
         cout << "target found.." << endl;
     else
         cout << "target not found.." << endl;
+
+    // binary Search
+    vector<int> brr1 = {10, 20, 30, 40, 50, 60, 70, 80, 90};
+    int targets = 70;
+    cout << "target at index: " << binarySearch(brr1, targets);
 
     return 0;
 }
