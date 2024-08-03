2796a427ae7f7b3a4ff602202f32693ce751d1d9
Ashu <ashuprince7337@gmail.com>
2024-08-03 21:41:29 +0530

    03-08-2024

--- week-04/3-searchingSorting2.cpp
+++ week-04/3-searchingSorting2.cpp
@@ -26,6 +26,58 @@ int pivot(vector<int> &nums)
     return -1;
 }
 
+// search in sorted and rotated array (33. leetcode)
+int pivotElement(vector<int> &arr)
+{
+    int n = arr.size();
+    int s = 0;
+    int e = n - 1;
+    int mid = s + (e - s) / 2;
+    while (s <= e)
+    {
+        if (s == e)
+            return s;
+        else if (mid + 1 < n && arr[mid] > arr[mid + 1])
+            return mid;
+        else if (mid - 1 >= 0 && arr[mid] < arr[mid - 1])
+            return mid - 1;
+        else if (arr[s] > arr[mid])
+            e = mid - 1;
+        else
+            s = mid + 1;
+        mid = s + (e - s) / 2;
+    }
+    return -1;
+}
+
+int binarySearch(vector<int> arr, int s, int e, int target)
+{
+    int mid = s + (e - s) / 2;
+    while (s <= e)
+    {
+        if (arr[mid] == target)
+            return mid;
+        else if (target > arr[mid])
+            s = mid + 1;
+        else if (target < arr[mid])
+            e = mid - 1;
+        mid = s + (e - s) / 2;
+    }
+    return -1;
+}
+
+int search(vector<int> arr, int target)
+{
+    int n = arr.size();
+    int ans = -1;
+    int pivotIndex = pivotElement(arr);
+    if (target >= arr[0] && target <= arr[pivotIndex])
+        ans = binarySearch(arr, 0, pivotIndex, target);
+    else if (target >= arr[pivotIndex + 1] && target <= arr[n - 1])
+        ans = binarySearch(arr, pivotIndex + 1, n - 1, target);
+    return ans;
+}
+
 int main()
 {
     // find pivot element( pivot can me the maximum element or minimum element based on requirement)
@@ -34,6 +86,12 @@ int main()
     cout << "pivot index: " << pivot(nums) << endl;
 
     // search in sorted and rotated array (33. leetcode)
+    vector<int> arr = {12, 14, 16, 2, 4, 6, 8, 10};
+    // vector<int> arr = {1, 3};
+    int target = 10;
+    cout << "pivot: " << pivotElement(arr) << endl;
+    cout << "binarysearch: " << binarySearch(arr, 0, 1, target) << endl;
+    cout << search(arr, target) << endl;
 
     return 0;
 }
