54937f2d0e1a18d79ad0a007c7c8d30b87653ae3
Ashu <ashuprince7337@gmail.com>
2024-08-09 20:23:12 +0530

    09-08-2024

--- week-04/4-searchingSorting3.cpp
+++ week-04/4-searchingSorting3.cpp
@@ -27,6 +27,30 @@ int divide(int dividend, int divisor)
     return ans;
 }
 
+// nearly sorted array
+int searchNearlySorted(vector<int> arr, int target)
+{
+    int n = arr.size();
+    int s = 0;
+    int e = n - 1;
+    int mid = s + (e - s) / 2;
+    while (s <= e)
+    {
+        if (mid - 1 >= 0 && arr[mid - 1] == target)
+            return mid - 1;
+        else if (arr[mid] == target)
+            return mid;
+        else if (mid + 1 < n && arr[mid + 1] == target)
+            return mid + 1;
+        else if (target > arr[mid])
+            s = mid + 2;
+        else
+            e = mid - 2;
+        mid = s + (e - s) / 2;
+    }
+    return -1;
+}
+
 int main()
 {
     // divide two numbers using binarySearch
@@ -40,6 +64,11 @@ int main()
     else if (divisor > 0 && dividend < 0 || divisor < 0 && dividend > 0)
         ans = 0 - ans;
     cout << ans << endl;
+
+    // nearly sorted array
+    vector<int> arr = {20, 10, 30, 50, 40, 70, 60};
+    int target = 70;
+    cout << searchNearlySorted(arr, target) << endl;
 
     return 0;
 }
