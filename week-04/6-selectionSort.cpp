5f0838e7a09497f1831bd5e3b9c385770f381b4e
Ashu <ashuprince7337@gmail.com>
2024-08-12 23:14:12 +0530

    12-08-2024

--- week-04/6-selectionSort.cpp
+++ week-04/6-selectionSort.cpp
@@ -2,10 +2,37 @@
 #include <vector>
 using namespace std;
 
+// print array
+void print(vector<int> v)
+{
+    for (int i = 0; i < v.size(); i++)
+    {
+        cout << v[i] << " ";
+    }
+}
+
+// selectionSort
+void selectionSort(vector<int> &v)
+{
+    int n = v.size();
+    for (int i = 0; i < n - 1; i++)
+    {
+        int minIndex = i;
+        for (int j = i + 1; j < n; j++)
+        {
+            if (v[j] < v[minIndex])
+                minIndex = j;
+        }
+        swap(v[i], v[minIndex]);
+    }
+}
+
 int main()
 {
     // selection sort
-    
+    vector<int> v = {44, 33, 55, 22, 11};
+    selectionSort(v);
+    print(v);
 
     return 0;
 }
