cf0a7589acd2cb7376263c1cfe345b1fb9c9f3cf
Ashu <ashuprince7337@gmail.com>
2024-08-16 11:30:57 +0530

    16-08-2024

---
+++ week-04/HOMEWORK/Q2.cpp
@@ -0,0 +1,11 @@
+// find k closest Element(658. leetcode)
+#include <iostream>
+#include <vector>
+using namespace std;
+int main()
+{
+    // find k colsest element(658. leetcode)
+    
+
+    return 0;
+}
