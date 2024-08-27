328df472e666bc01d42e09deff580437ffc1730d
Ashu <ashuprince7337@gmail.com>
2024-08-27 14:02:16 +0530

    27-08-2024

--- week-04/HOMEWORK/Q3.cpp
+++ week-04/HOMEWORK/Q3.cpp
@@ -6,15 +6,16 @@ using namespace std;
 // binary search
 int bs(vector<int> &arr, int s, int e, int x)
 {
+    int mid = s + (e - s) / 2;
     while (s <= e)
     {
-        int mid = s + (e - s) / 2;
         if (arr[mid] == x)
             return mid;
         else if (x > arr[mid])
             s = mid + 1;
         else
             e = mid - 1;
+        mid = s + (e - s) / 2;
     }
     return -1;
 }
@@ -23,12 +24,14 @@ int expoSearch(vector<int> &arr, int x)
 {
     int n = arr.size();
     if (arr[0] == x)
+    {
         return 0;
+    }
     int i = 1;
     while (i < n && arr[i] <= x)
     {
-        // i *= 2;
-        i <<= 1;
+        i *= 2;
+        // i <<= 1;
     }
     return bs(arr, i / 2, min(i, n - 1), x);
 }
@@ -37,9 +40,9 @@ int main()
 {
     // exponential search(doubling,galloping,strazik search)&unbounded binary search
     vector<int> arr = {3, 4, 5, 6, 11, 13, 14, 15, 56, 70};
-    int x = 56;
+    int x = 11;
     int ans = expoSearch(arr, x);
-    cout << ans << endl;
+    cout << "answer is at index: " << ans << endl;
 
     return 0;
 }
