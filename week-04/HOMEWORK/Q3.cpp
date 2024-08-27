390053a7841a9018c4284bf8a371c0f371d17ce8
Ashu <ashuprince7337@gmail.com>
2024-08-27 22:06:43 +0530

    27-08-2024

--- week-04/HOMEWORK/Q3.cpp
+++ week-04/HOMEWORK/Q3.cpp
@@ -35,6 +35,33 @@ int expoSearch(vector<int> &arr, int x)
     }
     return bs(arr, i / 2, min(i, n - 1), x);
 }
+// find the element in an infinite sorted array
+int binarySearch(vector<int> &brr, int s, int e, int &k)
+{
+    int mid = s + (e - s) / 2;
+    while (s <= e)
+    {
+        if (brr[mid] == k)
+            return mid;
+        else if (k > brr[mid])
+            s = mid + 1;
+        else
+            e = mid - 1;
+        mid = s + (e - s) / 2;
+    }
+    return -1;
+}
+int exponential(vector<int> &brr, int &k)
+{
+    int i = 0;
+    int j = 1;
+    while (brr[j] < k)
+    {
+        i = j;
+        j <<= 1;
+    }
+    return binarySearch(brr, i, j, k);
+}
 
 int main()
 {
@@ -42,7 +69,13 @@ int main()
     vector<int> arr = {3, 4, 5, 6, 11, 13, 14, 15, 56, 70};
     int x = 11;
     int ans = expoSearch(arr, x);
-    cout << "answer is at index: " << ans << endl;
+    cout << "exponential binary search is at index: " << ans << endl;
+
+    // find the element in an infinite sorted array
+    vector<int> brr = {3, 4, 5, 6, 11, 13, 14, 15, 56, 70};
+    int k = 4;
+    int ae = exponential(brr, k);
+    cout << "unbounded binary search of " << k << " is at index: " << ae << endl;
 
     return 0;
 }
